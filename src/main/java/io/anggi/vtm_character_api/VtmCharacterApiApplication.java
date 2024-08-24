a03aaed69fd0d999fbb231a2f5a82462a974b72c
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-08-24 19:34:13 +0300

    Initial commit

---
+++ src/main/java/io/anggi/vtm_character_api/VtmCharacterApiApplication.java
@@ -0,0 +1,13 @@
+package io.anggi.vtm_character_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtmCharacterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtmCharacterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/anggi/vtm_character_api/VtmCharacterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.anggi.vtm_character_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtmCharacterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
