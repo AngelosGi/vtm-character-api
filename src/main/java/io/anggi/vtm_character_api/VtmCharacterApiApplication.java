74ac30e42856a490dba6d2af32a419e3da6eae6e
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-08-24 22:10:55 +0300

    Chore: format

--- src/main/java/io/anggi/vtm_character_api/VtmCharacterApiApplication.java
+++ src/main/java/io/anggi/vtm_character_api/VtmCharacterApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VtmCharacterApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VtmCharacterApiApplication.class, args);
 	}
-
 }
