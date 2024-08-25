22bc2f77f68c1c3884a8ce9b1f44b67e413e68fb
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-08-25 11:10:56 +0300

    Feat: add Enums for predator type and clans (I will recreate later with classes)

---
+++ src/main/java/io/anggi/vtm_character_api/model/Enums.java
@@ -0,0 +1,12 @@
+package io.anggi.vtm_character_api.model;
+
+public class Enums {
+    public enum PredatorType {
+        ALLEYCAT, BAGGER, BLOOD_LEECH, CLEAVER, CONSENSUS_BREAKER, FARMER, OSIRIS, SANDMAN, SCENE_QUEEN, SIREN
+    }
+
+
+    public enum Clan {
+        BRUJAH, GANGREL, MALKAVIAN, NOSFERATU, TOREADOR, TREMERE, VENTRUE, CAITIFF, THIN_BLOOD
+    }
+}
