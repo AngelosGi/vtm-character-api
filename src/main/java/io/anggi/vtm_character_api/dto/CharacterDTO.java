7812d849c3902b850855367ac40da6c781a809ab
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-08-25 11:43:32 +0300

    Feat: Add more completed Character entity and DTO from the character sheet.

--- src/main/java/io/anggi/vtm_character_api/dto/CharacterDTO.java
+++ src/main/java/io/anggi/vtm_character_api/dto/CharacterDTO.java
@@ -1,13 +1,37 @@
 package io.anggi.vtm_character_api.dto;
 
-import io.anggi.vtm_character_api.model.Clan;
+import io.anggi.vtm_character_api.model.Enums.Clan;
+import io.anggi.vtm_character_api.model.Enums.PredatorType;
 import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
 
 @Data
 public class CharacterDTO {
     private Long id;
     private String name;
+    private String concept;
+    private String sire;
+    private String ambition;
+    private String desire;
     private Clan clan;
+    private PredatorType predatorType;
     private Integer generation;
+    private AttributesDTO attributes;
+    private SkillsDTO skills;
+    private Map<String, Integer> disciplines;
+    private Integer humanity;
     private Integer bloodPotency;
+    private List<String> chronicleTenets;
+    private List<TouchstoneConvictionDTO> touchstonesAndConvictions;
+    private String clanBane;
+    private Map<String, Integer> merits;
+    private Map<String, Integer> flaws;
+    private Map<String, Integer> backgrounds;
+    private Integer experience;
+
+    // Derived attributes
+    private Integer health;
+    private Integer willpower;
 }
--- src/main/java/io/anggi/vtm_character_api/model/Character.java
+++ src/main/java/io/anggi/vtm_character_api/model/Character.java
@@ -1,24 +1,85 @@
 package io.anggi.vtm_character_api.model;
+import io.anggi.vtm_character_api.model.Enums.PredatorType;
+import io.anggi.vtm_character_api.model.Enums.Clan;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
+import java.util.List;
+import java.util.Map;
 
 @Data
 @Entity
 @Table(name = "characters")
 public class Character {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String name;
+    private String concept;
+    private String sire;
+    private String ambition;
+    private String desire;
 
     @Enumerated(EnumType.STRING)
     private Clan clan;
 
+    @Enumerated(EnumType.STRING)
+    private PredatorType predatorType;
+
     private Integer generation;
 
+    @Embedded
+    private Attributes attributes;
+
+    @Embedded
+    private Skills skills;
+
+    @ElementCollection
+    @CollectionTable(name = "character_disciplines")
+    @MapKeyColumn(name = "discipline_name")
+    @Column(name = "discipline_level")
+    private Map<String, Integer> disciplines;
+
+    private Integer humanity;
     private Integer bloodPotency;
+
+    @ElementCollection
+    @CollectionTable(name = "character_chronicle_tenets")
+    @Column(name = "tenet")
+    private List<String> chronicleTenets;
+
+    @OneToMany(mappedBy = "character", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TouchstoneConviction> touchstonesAndConvictions;
+
+    private String clanBane;
+
+    @ElementCollection
+    @CollectionTable(name = "character_merits")
+    @MapKeyColumn(name = "merit_name")
+    @Column(name = "merit_level")
+    private Map<String, Integer> merits;
+
+    @ElementCollection
+    @CollectionTable(name = "character_flaws")
+    @MapKeyColumn(name = "flaw_name")
+    @Column(name = "flaw_level")
+    private Map<String, Integer> flaws;
+
+    @ElementCollection
+    @CollectionTable(name = "character_backgrounds")
+    @MapKeyColumn(name = "background_name")
+    @Column(name = "background_level")
+    private Map<String, Integer> backgrounds;
+
+    private Integer experience;
+
+    // Derived attributes
+    public Integer getHealth() {
+        return attributes.getPhysical().getStamina() + 3;
+    }
+
+    public Integer getWillpower() {
+        return attributes.getSocial().getComposure() + attributes.getMental().getResolve();
+    }
 }
